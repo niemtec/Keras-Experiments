2708e22cca3d616408b9a27a348c674fae575031
niemtec <janiemiec20@gmail.com>
2018-07-15 21:21:53 +0200

    Added the class labels

--- TensorFlow Tutorials/Basic-Classification.py
+++ TensorFlow Tutorials/Basic-Classification.py
@@ -11,3 +11,7 @@ print(tf.__version__)
 fashion_mnist = keras.datasets.fashion_mnist
 # Load the dataset
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# Class names for the dataset (not natively included)
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
