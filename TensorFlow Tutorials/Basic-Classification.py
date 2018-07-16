a10fa5b18285a86c35468b195100a834e68a1b6d
niemtec <janiemiec20@gmail.com>
2018-07-16 12:02:09 +0200

    Added printer to show predictions the model makes

--- TensorFlow Tutorials/Basic-Classification.py
+++ TensorFlow Tutorials/Basic-Classification.py
@@ -65,4 +65,31 @@ model.fit(train_images, train_labels, epochs=5, verbose=2)  # verbose 0 - no log
 
 # Evaluate accuracy
 test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Test Accuracy', test_acc)
+print('Test Accuracy: ', test_acc)
+
+# MAKE PREDICTIONS
+predictions = model.predict(test_images)
+# Select the highest confidence score
+print("Predicted Label: ", np.argmax(predictions[0]))
+print("Test Label: ", test_labels[0])
+
+# Plot the first 25 test images, their predicted label, and the true label
+# Color correct predictions in green, incorrect predictions in red
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid('off')
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    predicted_label = np.argmax(predictions[i])
+    true_label = test_labels[i]
+    if predicted_label == true_label:
+        color = 'green'
+    else:
+        color = 'red'
+    plt.xlabel("{} ({})".format(class_names[predicted_label],
+                                class_names[true_label]),
+               color=color)
+#plt.show()
+
