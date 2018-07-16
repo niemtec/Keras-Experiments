d6bceb7c4f8a7b2ab13292cc6d02d74643326522
niemtec <janiemiec20@gmail.com>
2018-07-16 11:23:24 +0200

    Added layer modification code

--- TensorFlow Tutorials/Basic-Classification.py
+++ TensorFlow Tutorials/Basic-Classification.py
@@ -1,5 +1,6 @@
 # TensorFlow and tf.keras
 import tensorflow as tf
+from keras.legacy import layers
 from tensorflow import keras
 
 # Helper libraries
@@ -54,3 +55,14 @@ model.add(keras.layers.Dense(64, activation='relu'))
 model.add(keras.layers.Dense(64, activation='relu'))
 # Add a softmax layer with 10 output units
 model.add(keras.layers.Dense(10, activation='softmax'))
+
+# Create a sigmoid layer:
+layers.Dense(64, activation='sigmoid')
+# A linear layer with L1 regularization of factor 0.01 applied to the kernel matrix:
+layers.Dense(64, kernel_regularizer=keras.regularizers.l1(0.01))
+# A linear layer with L2 regularization of factor 0.01 applied to the bias vector:
+layers.Dense(64, bias_regularizer=keras.regularizers.l2(0.01))
+# A linear layer with a kernel initialized to a random orthogonal matrix:
+layers.Dense(64, kernel_initializer='orthogonal')
+# A linear layer with a bias vector initialized to 2.0s:
+layers.Dense(64, bias_initializer=keras.initializers.constant(2.0))
