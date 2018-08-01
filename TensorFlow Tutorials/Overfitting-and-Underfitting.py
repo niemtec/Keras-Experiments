b9b36519babb024134e6c1621eefffbbce982e96
Jakub Adrian Niemiec <janiemiec20@gmail.com>
2018-08-01 14:39:43 +0100

    Add L2 Optimiser

--- TensorFlow Tutorials/Overfitting-and-Underfitting.py
+++ TensorFlow Tutorials/Overfitting-and-Underfitting.py
@@ -87,3 +87,48 @@ bigger_history = bigger_model.fit(train_data, train_labels,
                                   verbose=2)
 
 
+# Plot training and validation loss
+def plot_history(histories, key='binary_crossentropy'):
+    plt.figure(figsize=(16,10))
+
+    for name, history in histories:
+        val = plt.plot(history.epoch, history.history['val_'+key],
+                       '--', label=name.title()+' Val')
+        plt.plot(history.epoch, history.history[key], color=val[0].get_color(),
+                 label=name.title()+' Train')
+
+    plt.xlabel('Epochs')
+    plt.ylabel(key.replace('_',' ').title())
+    plt.legend()
+
+    plt.xlim([0,max(history.epoch)])
+    plt.savefig('Plot-History.png')
+    plt.show()
+
+
+plot_history([('baseline', baseline_history),
+              ('smaller', smaller_history),
+              ('bigger', bigger_history)])
+
+# Adding weight regularisation
+l2_model = keras.models.Sequential([
+    keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.001),
+                       activation=tf.nn.relu, input_shape=(10000,)),
+    keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.001),
+                       activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+l2_model.compile(optimizer='adam',
+                 loss='binary_crossentropy',
+                 metrics=['accuracy', 'binary_crossentropy'])
+
+l2_model_history = l2_model.fit(train_data, train_labels,
+                                epochs=20,
+                                batch_size=512,
+                                validation_data=(test_data, test_labels),
+                                verbose=2)
+
+# Printing plot history to show L2 regularisation
+plot_history([('baseline', baseline_history),
+              ('l2', l2_model_history)])
