4a4b3fc6b91ec5a0a92b256fbb5e2c80fd779c09
Jakub Adrian Niemiec <janiemiec20@gmail.com>
2018-08-01 19:44:39 +0100

    Get example dataset

---
+++ TensorFlow Tutorials/Save-and-Restore-Models.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+
+import tensorflow as tf
+from tensorflow import keras
+
+tf.__version__
+
+# Get example dataset
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+train_labels = train_labels[:1000]
+test_labels = test_labels[:1000]
+
+train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
+test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
