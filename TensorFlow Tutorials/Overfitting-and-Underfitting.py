bb90802f8ffbdf153cd13b5a109e25698d96b5cb
Jakub Adrian Niemiec <janiemiec20@gmail.com>
2018-08-01 12:21:00 +0100

    Download the demo data

---
+++ TensorFlow Tutorials/Overfitting-and-Underfitting.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+# Multi-hot encoding the sentences (to quickly overfit the training set for demonstration)
+NUM_WORDS = 10000
+
+(train_data, train_labels), (test_data, test_labels) = keras.datasets.imdb.load_data(num_words=NUM_WORDS)
+
+def multi_hot_sequences(sequences, dimension):
+    # Create an all-zero matrix of shape (len(sequences), dimension)
+    results = np.zeros((len(sequences), dimension))
+    for i, word_indices in enumerate(sequences):
+        results[i, word_indices] = 1.0 # set specific indices of results[i] to 1s
+    return results
+
+train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
+test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
+
+plt.plot(train_data[0])
+plt.show()
