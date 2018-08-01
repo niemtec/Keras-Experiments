24e1ef296796fe74d7664d5fe5f739fb1133575d
Jakub Adrian Niemiec <janiemiec20@gmail.com>
2018-08-01 12:55:03 +0100

    Create smaller model

--- TensorFlow Tutorials/Overfitting-and-Underfitting.py
+++ TensorFlow Tutorials/Overfitting-and-Underfitting.py
@@ -11,12 +11,14 @@ NUM_WORDS = 10000
 
 (train_data, train_labels), (test_data, test_labels) = keras.datasets.imdb.load_data(num_words=NUM_WORDS)
 
+
 def multi_hot_sequences(sequences, dimension):
     # Create an all-zero matrix of shape (len(sequences), dimension)
     results = np.zeros((len(sequences), dimension))
     for i, word_indices in enumerate(sequences):
-        results[i, word_indices] = 1.0 # set specific indices of results[i] to 1s
+        results[i, word_indices] = 1.0  # set specific indices of results[i] to 1s
     return results
+
 
 train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
 test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
@@ -29,10 +31,37 @@ baseline_model = keras.Sequential([
     keras.layers.Dense(16, activation=tf.nn.relu, input_shape=(10000,)),
     keras.layers.Dense(16, activation=tf.nn.relu),
     keras.layers.Dense(1, activation=tf.nn.sigmoid)
-    ])
+])
 
 baseline_model.compile(optimizer='adam',
                        loss='binary_crossentropy',
                        metrics=['accuracy', 'binary_crossentropy'])
 
 baseline_model.summary()
+
+baseline_history = baseline_model.fit(train_data,
+                                      train_labels,
+                                      epochs=20,
+                                      batch_size=512,
+                                      validation_data=(test_data, test_labels),
+                                      verbose=2)
+
+# Creating the smaller model
+smaller_model = keras.Sequential([
+    keras.layers.Dense(4, activation=tf.nn.relu, input_shape=(10000,)),
+    keras.layers.Dense(4, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+smaller_model.compile(optimizer='adam',
+                      loss='binary_crossentropy',
+                      metrics=['accuracy', 'binary_crossentropy'])
+
+smaller_model.summary()
+
+smaller_history = smaller_model.fit(train_data,
+                                    train_labels,
+                                    epochs=20,
+                                    batch_size=512,
+                                    validation_data=(test_data, test_labels),
+                                    verbose=2)
